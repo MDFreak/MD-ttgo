18be508d112b65e6c8450ed68735ba9c74dd76ed
MDFreak <inbox@martin-dorfner.at>
2023-11-14 00:28:54 +0100

    ones more

--- src/Button2.cpp
+++ src/Button2.cpp
@@ -5,8 +5,8 @@
 */
 /////////////////////////////////////////////////////////////////
 
-#include "Arduino.h"
-#include "Button2.h"
+#include <Arduino.h>
+#include <Button2.h>
 
 /////////////////////////////////////////////////////////////////
 
@@ -19,33 +19,33 @@ Button2::Button2(byte attachTo, byte buttonMode /*= INPUT_PULLUP*/, unsigned int
 /////////////////////////////////////////////////////////////////
 
 bool Button2::operator==(Button2 &rhs) {
-      return (this==&rhs);    
+      return (this==&rhs);
 }
-      
+
 /////////////////////////////////////////////////////////////////
 
 void Button2::setDebounceTime(unsigned int ms) {
       debounce_time_ms = ms;
     }
-    
+
 /////////////////////////////////////////////////////////////////
 
 void Button2::setChangedHandler(CallbackFunction f) {
-  change_cb = f; 
+  change_cb = f;
 }
-    
+
 /////////////////////////////////////////////////////////////////
 
 void Button2::setPressedHandler(CallbackFunction f) {
-  pressed_cb = f; 
+  pressed_cb = f;
 }
 
 /////////////////////////////////////////////////////////////////
 
 void Button2::setReleasedHandler(CallbackFunction f) {
-  released_cb = f; 
+  released_cb = f;
 }
-        
+
 /////////////////////////////////////////////////////////////////
 
 void Button2::setClickHandler(CallbackFunction f) {
@@ -87,7 +87,7 @@ unsigned int Button2::wasPressedFor() {
 boolean Button2::isPressed() {
   return (state == LOW);
 }
-    
+
 /////////////////////////////////////////////////////////////////
 
 unsigned int Button2::getNumberOfClicks() {
@@ -120,11 +120,11 @@ void Button2::loop() {
     down_time_ms = millis() - down_ms;
     // is it beyond debounce time?
     if (down_time_ms >= debounce_time_ms) {
-      // trigger release        
+      // trigger release
       if (change_cb != NULL) change_cb (*this);
       if (released_cb != NULL) released_cb (*this);
       // trigger tap
-      if (tap_cb != NULL) tap_cb (*this);        
+      if (tap_cb != NULL) tap_cb (*this);
       // was it a longclick? (preceeds single / double / triple clicks)
       if (down_time_ms >= LONGCLICK_MS) {
         longclick_detected = true;
@@ -133,10 +133,10 @@ void Button2::loop() {
 
   // trigger pressed event (after debounce has passed)
   } else if (state == LOW && !pressed_triggered && (millis() - down_ms >= debounce_time_ms)) {
-    if (change_cb != NULL) change_cb (*this);      
+    if (change_cb != NULL) change_cb (*this);
     if (pressed_cb != NULL) pressed_cb (*this);
     pressed_triggered = true;
-  
+
   // is the button pressed and the time has passed for multiple clicks?
   } else if (state == HIGH && millis() - click_ms > DOUBLECLICK_MS) {
     // was there a longclick?
@@ -146,21 +146,21 @@ void Button2::loop() {
         last_click_type = LONG_CLICK;
         if (long_cb != NULL) long_cb (*this);
       }
-      longclick_detected = false;      
+      longclick_detected = false;
     // determine the number of single clicks
     } else if (click_count > 0) {
       switch (click_count) {
-        case 1: 
+        case 1:
           last_click_type = SINGLE_CLICK;
           if (click_cb != NULL){
               click_cb (*this);
-          } 
+          }
           break;
-         case 2: 
+         case 2:
           last_click_type = DOUBLE_CLICK;
           if (double_cb != NULL) double_cb (*this);
           break;
-         case 3: 
+         case 3:
           last_click_type = TRIPLE_CLICK;
           if (triple_cb != NULL) triple_cb (*this);
           break;
@@ -168,7 +168,7 @@ void Button2::loop() {
     }
     click_count = 0;
     click_ms = 0;
-  }   
+  }
 }
 
 /////////////////////////////////////////////////////////////////
